cac240119402d1aebd2a6cfbfe4298190970bcd4
benjaminmeredith <benjamin.meredith97@gmail.com>
2023-06-06 14:37:50 -0700

    Adding first files to githu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * An example program
+ *
+ * @author benjamin Meredith
+ * @version 1.0
+ */
 public class Main
 {
+
+    /**
+     * Entry point for application
+     * @param args command line args
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
